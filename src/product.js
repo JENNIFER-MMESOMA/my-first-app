7f0b9075474c887b3de7e2374b1e64ebd07e3800
JENNIFER-MMESOMA <adammesomachi@gmail.com>
2024-09-03 16:26:39 -0700

    product

---
+++ src/product.js
@@ -0,0 +1,29 @@
+const products = [
+    {
+      id: 1,
+      name: "shoe",
+      description: "A pair of shoe",
+      image:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/c20afd60-b230-4815-bfd2-6768c875f6cd/air-force-1-07-shoes-0XGfD7.png",
+      price: "$30000",
+    },
+    {
+      id: 2,
+      name: "Glass",
+      description: "Anti blue ray glasses",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrWNejXfPgaOO6m1ZOKObb3nbGeRNaIXQqxw&s",
+      price: "10000",
+    },
+    {
+      id: 3,
+      name: "Bucket Hat",
+      description: "A bucket hat to prevent sunburn",
+      image:
+        "https://weardiop.com/cdn/shop/products/the-flower-boy-bucket-hat-bucket-hat-diop-483827_600x.jpg?v=1673292700",
+      price: "$60000",
+    },
+  ];
+  
+  export default products;
+  
